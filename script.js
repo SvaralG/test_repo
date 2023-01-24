194a79d6fa879f421a62081e6de3736842e57b6c
SvaralG <pulatova@bk.ru>
2023-01-24 23:34:44 +0100

    dynamic types

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-let x = 10
-let y = 6
+let newYear = "New year - big party";
+console.log(newYear);
+console.log(typeof newYear);
+
+newYear = 2023;
+console.log (newYear);
+console.log (typeof newYear);
