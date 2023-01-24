d12c746651295c08ef5f7775f63b1aa7c2b114e9
SvaralG <pulatova@bk.ru>
2023-01-25 00:28:09 +0100

    GAP

---
+++ hw-5.3/task_score.js
@@ -0,0 +1,12 @@
+let introduction = 10
+let git = 50;
+let js = 90;
+let total = introduction + git + js
+let gradePointAverage = total/3
+
+console.log(`GAP ${gradePointAverage}`);
+
+let studentName = "Petr"
+let studentSurname = "Ivanov"
+let student = studentName + studentSurname
+console.log(`GAP of student ${student} ${gradePointAverage} points`);
